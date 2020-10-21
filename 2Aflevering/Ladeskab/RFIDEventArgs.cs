f46380fb509868e1231b4f7761dcbaa58e477cce
thomasgl710 <thomasgl10@gmail.com>
2020-10-21 08:46:54 +0200

    check

---
+++ 2Aflevering/Ladeskab/RFIDEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskab
+{
+    public class RFIDEventArgs : EventArgs
+    {
+        public double RFID { get; set; }
+    }
+}
